254acf1b1a025dafda0dbf553f1cc887fed5c295
Per Hallström <hallstroem@gmail.com>
2014-12-15 17:41:25 +1100

    Rewrite step 2: warrior dies later, but does not recover enough health

--- codes.rb
+++ codes.rb
@@ -1,22 +1,31 @@
 class Player
 
   def play_turn(warrior)
-    
     @last_health = warrior.health if @last_health.nil?
     @walking_direction = :backward if @walking_direction.nil?
+    
+    is_under_attack = (warrior.health < @last_health)
+    is_health_low = (warrior.health < 10)
 
     feeling = warrior.feel(@walking_direction)
+    @walking_direction = :forward if feeling.wall?
 
-    if feeling.wall?
-      @walking_direction = :forward
-    elsif feeling.captive?
+    if feeling.captive?
       warrior.rescue!(@walking_direction)
     elsif feeling.enemy?
       warrior.attack!(@walking_direction)
     else 
-      warrior.walk!(@walking_direction)
+      if is_health_low
+        if is_under_attack
+          warrior.walk!(:backward)
+        else
+          warrior.rest!
+        end
+      else
+        warrior.walk!(@walking_direction)
+      end
     end
-
+    @last_health = warrior.health
   end
 end
 
