c18376b8f90de003ec86cca2c05f02a01425b1c4
Per Hallström <hallstroem@gmail.com>
2014-12-15 17:28:30 +1100

    Rewrite step 1: warrior dies

--- codes.rb
+++ codes.rb
@@ -1,37 +1,22 @@
 class Player
 
   def play_turn(warrior)
-    @health = 0 if @health.nil? 
-    @has_rescued = false if @has_rescued.nil?
-    @needed_health_level = 10 if @needed_health_level.nil?
+    
+    @last_health = warrior.health if @last_health.nil?
+    @walking_direction = :backward if @walking_direction.nil?
 
-    feel_back = warrior.feel(:backward)
+    feeling = warrior.feel(@walking_direction)
 
-    if !@has_rescued
-      if !feel_back.wall?
-        if feel_back.captive?
-          warrior.rescue!(:backward)
-          @has_rescued = true
-        else  
-          warrior.walk!(:backward)  
-        end
-      end
-    elsif warrior.feel.empty?
-      if warrior.health < @needed_health_level
-        @needed_health_level = 20 if @needed_health_level = 10
-        if warrior.health >= @health # if we are okay, not taking damage
-          @needed_health_level = 10 if warrior.health >= 18
-          warrior.rest!
-        else # taking damage
-          warrior.walk!(:backward)
-        end
-      else
-        warrior.walk!
-      end
-    else
-        warrior.attack!
+    if feeling.wall?
+      @walking_direction = :forward
+    elsif feeling.captive?
+      warrior.rescue!(@walking_direction)
+    elsif feeling.enemy?
+      warrior.attack!(@walking_direction)
+    else 
+      warrior.walk!(@walking_direction)
     end
-    @health  = warrior.health
+
   end
 end
-  
+
