f1f3ad79266c1e0182160ac368594e89cff5fbad
Per Hallström <hallstroem@gmail.com>
2014-12-15 17:50:45 +1100

    Rewrite step 3: warrior wins, yay

--- codes.rb
+++ codes.rb
@@ -1,11 +1,16 @@
 class Player
 
+  def is_health_low(warrior)
+    @need_full_rest = false if warrior.health > 18 or @need_full_rest.nil?
+    @need_full_rest = true  if warrior.health < 10
+    return warrior.health < (@need_full_rest ? 20 : 10)
+  end
+
   def play_turn(warrior)
     @last_health = warrior.health if @last_health.nil?
     @walking_direction = :backward if @walking_direction.nil?
     
     is_under_attack = (warrior.health < @last_health)
-    is_health_low = (warrior.health < 10)
 
     feeling = warrior.feel(@walking_direction)
     @walking_direction = :forward if feeling.wall?
@@ -15,7 +20,7 @@ class Player
     elsif feeling.enemy?
       warrior.attack!(@walking_direction)
     else 
-      if is_health_low
+      if is_health_low(warrior)
         if is_under_attack
           warrior.walk!(:backward)
         else
