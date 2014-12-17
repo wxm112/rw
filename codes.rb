8282061b5c23d8fa227941dde9fbd2d40504db38
May Wang <wxm112@me.com>
2014-12-17 17:28:12 +1100

    Level 7 done!

--- codes.rb
+++ codes.rb
@@ -8,27 +8,21 @@ class Player
 
   def play_turn(warrior)
     @last_health = warrior.health if @last_health.nil?
-    @walking_direction = :backward if @walking_direction.nil?
     
     is_under_attack = (warrior.health < @last_health)
 
-    feeling = warrior.feel(@walking_direction)
-    @walking_direction = :forward if feeling.wall?
-
-    if feeling.captive?
-      warrior.rescue!(@walking_direction)
-    elsif feeling.enemy?
-      warrior.attack!(@walking_direction)
-    else 
-      if is_health_low(warrior)
-        if is_under_attack
-          warrior.walk!(:backward)
-        else
-          warrior.rest!
-        end
+    if warrior.feel.wall?
+      warrior.pivot!
+    elsif warrior.feel.enemy?
+      warrior.attack!
+    elsif is_health_low(warrior)
+      if is_under_attack
+        warrior.walk!(:backward)
       else
-        warrior.walk!(@walking_direction)
+        warrior.rest!
       end
+    else
+      warrior.walk!
     end
     @last_health = warrior.health
   end
