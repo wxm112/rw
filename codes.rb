ab686ee33e6286195d383bfb30b76dbb1ccd62bc
May Wang <wxm112@me.com>
2014-12-21 11:34:12 +1100

    Level 8

--- codes.rb
+++ codes.rb
@@ -1,30 +1,12 @@
 class Player
 
-  def is_health_low(warrior)
-    @need_full_rest = false if warrior.health > 18 or @need_full_rest.nil?
-    @need_full_rest = true  if warrior.health < 10
-    return warrior.health < (@need_full_rest ? 20 : 10)
-  end
-
   def play_turn(warrior)
-    @last_health = warrior.health if @last_health.nil?
-    
-    is_under_attack = (warrior.health < @last_health)
-
-    if warrior.feel.wall?
-      warrior.pivot!
-    elsif warrior.feel.enemy?
-      warrior.attack! # comment just so we can push a change ...
-    elsif is_health_low(warrior)
-      if is_under_attack
-        warrior.walk!(:backward)
-      else
-        warrior.rest!
-      end
+    if warrior.feel.captive?
+      warrior.rescue!
+    elsif warrior.look[1].enemy? 
+      warrior.shoot!
     else
       warrior.walk!
     end
-    @last_health = warrior.health
-  end
-
+  end   
 end
